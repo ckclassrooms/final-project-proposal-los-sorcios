7597024ca5d02dc83b43bb55e08d9fe86c86f7d9
Emilio Ingenito <emilio.ingenito@icloud.com>
2022-11-13 03:16:01 -0600

    labeling and image uploading working

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,11 @@ import {
 } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { supabase } from './supabaseClient';
-import { decode } from 'base64-arraybuffer';
-
-
 
 function App() {
 
   const [session, setSession] = useState(null);
   const [file, setFile] = useState(null);
-  const [immagine, setImmagine] = useState(null);
 
   // Manage session with Github 
   useEffect(() => {
@@ -29,94 +25,35 @@ function App() {
     })
   }, [])
 
-  const getImg = async() =>{
-    const { data, error } = await supabase
-    .storage
-    .from('images')
-    .download(file.name)
-    console.log(data)
-  }
-
   const callLambda = async() =>{
-    /*
     const { data, error } = await supabase.functions.invoke('prova', {
-      body: { filename: file.name, filesize: file.size, filetype: file.type, base64: file.base64},
+      body: { name: file.name, size: file.size, type: file.type, base64: file.base64},
     })
     console.log(data)
-    */
-   //console.log(decode(file.base64))
-
-    const googleVisionApiUrl = "https://vision.googleapis.com/v1/images:annotate?key=" // here api key should be added
-
-    const request = {
-      "requests":[
-        {
-          "image":{
-            "content": file.base64
-          },
-          "features":[
-            {
-              "type":"LABEL_DETECTION",
-              "maxResults":1
-            }
-          ]
-        }
-      ]
-    }
-
-    const response = await fetch(googleVisionApiUrl, {
-      method : 'POST',
-      mode : 'cors',
-      body : JSON.stringify(request)
-    })
-
-    console.log(response.text)
-
-
-    const { data, error } = await supabase
-    .storage
-    .from('images')
-    .upload(file.name, file)
-    console.log(file)
-    /*
-    .upload(file.name, decode(file.base64), {
-      contentType: 'image/png'
-    })
-    */
   }
 
-  const invokeFunction = async() =>{
-
+  const uploadImage = async() =>{
     //get base 64 of image
     let base64;
     // Make new FileReader
     let reader = new FileReader();
-  
     // Convert the file to base64 text
     reader.readAsDataURL(file);
     reader.onload = () => {
       // from jpg image to base64
-      console.log("Called", reader);
-      base64 = reader.result;
+      base64 = reader.result.split(',')[1];
       let updatedFile = file;
       updatedFile.base64 = base64;
-      //console.log(base64);
-    
       // modify the file
       setFile(updatedFile);
-
       //call lambda with updated file
       callLambda();
     };
   }
 
-  const onFileChange = event => {
+  const setLocalFile = event => {
     setFile(event.target.files[0]);
   };
-
-  const onFileUpload = () => {
-    invokeFunction();
-  }
 
   return (
     <>
@@ -127,11 +64,8 @@ function App() {
         </Routes>
     </div>
     <div>
-      <input type="file" onChange={onFileChange} />
-      <button onClick={invokeFunction}>Upload!</button>
-    </div>
-    <div>
-    <button onClick={invokeFunction}>GetImg!</button>
+      <input type="file" onChange={setLocalFile} />
+      <button onClick={uploadImage}>Upload image</button>
     </div>
     </>
   );
--- supabase/functions/prova/index.ts
+++ supabase/functions/prova/index.ts
@@ -1,78 +1,66 @@
 import { serve } from 'https://deno.land/std@0.131.0/http/server.ts'
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.0.0'
-import { Vision } from "https://googleapis.deno.dev/v1/vision:v1.ts";
-import type { CredentialsClient } from "https://googleapis.deno.dev/v1/vision:v1.ts";
-
-
-console.log(`Function "browser-with-cors" up and running!`)
+import { base64 } from "https://cdn.jsdelivr.net/gh/hexagon/base64@1/src/base64.js";
 
 serve(async (req) => {
-
+  
   const corsHeaders = {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Headers': 'authorization, content-type, x-client-info, apikey',
   }
 
-  console.log("here is the server")
-  // This is needed if you're planning to invoke your function from a browser.
   if (req.method === 'OPTIONS') {
     return new Response('ok', { headers: corsHeaders })
   } 
-
-  function getRequestHeaders(string){
-    return {Authorization: 'Bearer <access_token_value>'}
-  }
-
-  const cred: CredentialsClient = {
-    projectId: "12",
-    getRequestHeaders("str"),
-  };
-
-  const vision = new Vision("client", "string")
   
   try {
-    const image = await req.json()
-    const supabase_url = 'https://jsnykdczxbblcbkallpz.supabase.co'
-    const anon_key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpzbnlrZGN6eGJibGNia2FsbHB6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjY1Mzk0NjAsImV4cCI6MTk4MjExNTQ2MH0.ZUXIpeMfXssS8tKZBdqCeiTDNbg-_VuKg-vMpB6CwZ4'
-    const storage_url = 'https://app.supabase.com/project/jsnykdczxbblcbkallpz/storage/buckets'
-    const service_key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24ifQ.625_WdcF3KHqz5amU0x2X5WWHP-OEs_4qj0ssLNHzTs'
-    const google_apiKey = 'AIzaSyD6DVn3D1ay1TQ4DQ6d9hZrVi-9cU8yir8'
-    const data = {
-      message: image.base64,
-    }
-    
-    const supabaseClient = createClient(supabase_url, anon_key,
+    const file = await req.json()
+    // instantiate supabase client
+    const supabaseClient = createClient(
+      Deno.env.get('SUPABASE_URL') ?? '',
+      Deno.env.get('SUPABASE_ANON_KEY') ?? '',
       { global: { headers: { Authorization: req.headers.get('Authorization')! } } }
     )
 
+    // send request of labeling to GOOGLE_CLOUD_VISION
+    const request = {
+      "requests":[
+        {
+          "image":{
+            "content": file.base64
+          },
+          "features":[
+            {
+              "type":"LABEL_DETECTION",
+              "maxResults":1
+            }
+          ]
+        }
+      ]
+    }
+    const response = await fetch(
+      Deno.env.get('GOOGLE_VISION_URL') ?? '', {
+      method : 'POST',
+      mode : 'cors',
+      body : JSON.stringify(request)
+    });
 
+    const datas = await response.json();
+    const label = datas.responses[0].labelAnnotations[0].description
 
     // insert image row in the database
-    /*
-    const { error } = await supabaseClient
+    const { _ } = await supabaseClient
       .from('images')
-      .insert({ name: image.filename, size: image.filesize })
-    */
+      .insert({ name: file.name, size: file.size, label: label })
 
-    // upload image into a bucket
-    /*
-    var binary_string = window.atob(image.base64);
-    var len = binary_string.length;
-    var bytes = new Uint8Array(len);
-    for (var i = 0; i < len; i++) {
-        bytes[i] = binary_string.charCodeAt(i);
-    }
-    let img = bytes.buffer;
-    */
-    const { error } = await supabaseClient.storage
-      .from('images')
-      .upload(image.filename, image.buffer, {
-        contentType: 'image/jpg'
-      })
-    
-    
+    // insert image in the bucket
+    const { data, error } = await supabaseClient.storage
+    .from('images')
+    .upload( file.name, base64.toArrayBuffer(file.base64), {
+      contentType: 'image/jpg'})
+
     //craft the response to be sent to the client
-    return new Response(JSON.stringify(data), {
+    return new Response(JSON.stringify(label), {
       headers: { ...corsHeaders, 'Content-Type': 'application/json' },
       status: 200,
     })
@@ -84,7 +72,3 @@ serve(async (req) => {
   }
 })
 
-// curl -i --location --request POST 'http://localhost:54321/functions/v1/browser-with-cors' \
-//   --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24ifQ.625_WdcF3KHqz5amU0x2X5WWHP-OEs_4qj0ssLNHzTs' \
-//   --header 'Content-Type: application/json' \
-//   --data '{"name":"Functions"}'
