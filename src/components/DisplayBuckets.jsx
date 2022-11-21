b29c85c94538ae399cfcfa4e939d4ab6e102cfc3
Calderonio_Giuseppe <10630087@polimi.it>
2022-11-20 19:44:22 -0600

    added rating and 'hot or not' logic

--- src/components/DisplayBuckets.jsx
+++ src/components/DisplayBuckets.jsx
@@ -1,44 +1,77 @@
 import React from 'react'
 import { useState, useEffect } from "react";
 import { supabase } from '../supabaseClient';
+import { ShowBucket } from './ShowBucket'
 
 
 function DisplayBuckets({file, setFile}) {
-    const supabase_url = 'https://jsnykdczxbblcbkallpz.supabase.co/storage/v1/object/public/images/'
-    const [imgs, setImgs] = useState(null)
+
+    /**
+     * this attribute represents an array of buckets
+     */
     const [buckets, setBuckets] = useState(null)
-    /* 
-        fetch all the images from the database
-    */
-    const fetchImgs = async () => {
-        const { data, error } = await supabase
-            .storage
-            .from('images')
-            .list('', { offset: 1 })
-        setImgs(data)
-    }
-    /* 
-        fetch all the buckets from the database
-    */
+
+    /**
+     * this attribute represents the current label of the buclet displayed
+     */
+    const [label, setLabel] = useState(null)
+
+    /**
+     * this attribute represents an array containing all the poosible labels
+     */
+    const [labels, setLabels] = useState(null)
+
+    /**
+     * this attribute represents the index of the image to display given
+     * the bucket selected of the label "label"
+     */
+    const [imageIndex, setImageIndex] = useState(0)
+   
+    /**
+     * this funciton fetches all the images metadata from the
+     * database and sets an array of unique labels from it
+     */
     const fetchBuckets = async () => {
         const { data, error } = await supabase
         .from('images')
-        .select('label');
-        var bucks = new Set(data.map((singleImg, index) => singleImg.label))
-        var f_bucks = Array.from(bucks);
-        setBuckets(f_bucks)
+        .select('*');
+        console.log(data)
+        const newLabels = new Set(data.map((singleImg, index) => singleImg.label))
+        
+        setBuckets(Array.from(data))
+        setLabels(Array.from(newLabels))
+        
     }
 
+    /**
+     * this function handles a button press
+     * in particular it reset the image index to zero and
+     * set the label, such that the bucket can be displayed
+     * @param {*} el this is the label of the bucket to show
+     */
+    function handleButton(el){
+        setImageIndex(0)
+        setLabel(el)
+
+    }
+
+    /**
+     * this function is called at each render, and fethces the
+     * images metadata
+     */
     useEffect(() => {
         fetchBuckets();
-        fetchImgs();
       }, []);
+
 
     return (
         <div className='container'>
-        {buckets != null &&
-            buckets.map((el, index) => (<div key={index} ><button className='box'>{el}</button></div>))
+        {labels != null &&
+            labels.map((el, index) => (<div key={index} ><button className='box' onClick={() => handleButton(el)}>{el}</button></div>))
         }
+            <div>
+                {label != null && <ShowBucket bucket={label} imgs={buckets} imageIndex={imageIndex} setImageIndex={setImageIndex}></ShowBucket>}
+            </div>
         </div>
        
     )
