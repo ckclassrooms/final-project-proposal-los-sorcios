2570d0c05400ad1a114d1a7c8a118f77ec1da9dc
emilioingenito <72695814+emilioingenito@users.noreply.github.com>
2022-11-28 11:58:07 -0600

    working upld

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -7,7 +7,7 @@ function Landing() {
                 <h1>Welcome to LosSorcios!</h1>
                 This application allows you to upload images, rate them based on the category 
                 that our autmatic classifier decides, and to see the top ranked images!
-                <h1>A quick overview:</h1>
+                <h2>A quick overview:</h2>
                 <ul>
                     <li><b>Upload images</b>: upload any image you wish, but keep in mind that the explicit content detector is supervising ...</li>
                     <li><b>Display top images</b>: take a look at the most-liked images of this website!</li>
